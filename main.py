c29bd02b70980726011e7df3157691567919378a
Raphael Dias <86736451+Beelzebub3122@users.noreply.github.com>
2023-06-19 09:25:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask,render_template
 
-
- 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -12,7 +8,7 @@ app = Flask(__name__)
 # which tells the application which URL should call
 # the associated function.
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
+
 def index():
     name = 'NATHAN'
     return render_template("index.html",name =name)
@@ -23,4 +19,3 @@ if __name__ == '__main__':
     # on the local development server.
     app.run()
  
-# main driver func
